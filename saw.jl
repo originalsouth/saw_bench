7533a2f78467bc227e339fc523eac7e0027193c6
Michel Fruchart <michel.fruchart@ens-lyon.org>
2017-03-17 14:55:07 +0100

    Removing global variables and adding types to speed up the julia version.

--- saw.jl
+++ saw.jl
@@ -1,39 +1,37 @@
 #!/usr/bin/env julia
 
-NMAX=20
-n=0
-z=0.0
-rsq=0.0
-grid=zeros(Bool,2*NMAX+1,2*NMAX+1)
+const NMAX=20
 
-function mov(x,y,ln)
-    global grid
+function mov(grid::Array{Bool,2}, z::Float64, rsq::Float64, n::Int64, x::Int64,y::Int64,ln::Int64)
     if ln==n
-        global z+=1.0
-        global rsq+=(x-NMAX-1)^2+(y-NMAX-1)^2
+        z+=1.0
+        rsq+=(x-NMAX-1)^2+(y-NMAX-1)^2
     else
         grid[x,y]=true
         if !grid[x+1,y]
-            mov(x+1,y,ln+1)
+            grid, z, rsq = mov(grid, z, rsq, n, x+1, y, ln+1)
         end
         if !grid[x-1,y]
-            mov(x-1,y,ln+1)
+            grid, z, rsq = mov(grid, z, rsq, n, x-1, y, ln+1)
         end
         if !grid[x,y+1]
-            mov(x,y+1,ln+1)
+            grid, z, rsq = mov(grid, z, rsq, n, x, y+1, ln+1)
         end
         if !grid[x,y-1]
-            mov(x,y-1,ln+1)
+            grid, z, rsq = mov(grid, z, rsq, n, x, y-1, ln+1)
         end
         grid[x,y]=false
     end
+    return grid, z, rsq, n
 end
+
+grid=zeros(Bool,2*NMAX+1,2*NMAX+1)
 
 @printf("#n[1];z[2];<rsq>[3]\n");
 for n in 0:1:NMAX-1
     z=0.0
     rsq=0.0
-    mov(NMAX+1,NMAX+1,0)
+    grid, z, rsq = mov(grid, z, rsq, n, NMAX+1,NMAX+1,0)
     @printf("%d;%.17g;%.17g\n",n,z,rsq/z)
 end
 exit(0)
